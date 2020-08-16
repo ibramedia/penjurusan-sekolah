3b78858bc304718435c44261477c59a64d864fef
tfkproject <taufik.y2t@gmail.com>
2020-08-17 00:25:24 +0700

    Update breadcrumb.php

--- application/views/bk/_partials/breadcrumb.php
+++ application/views/bk/_partials/breadcrumb.php
@@ -10,7 +10,7 @@ if(empty($sector2)){
         <small><?php echo ucfirst($sector2)?></small>
       </h1>
       <ol class="breadcrumb">
-        <li><?php echo ucfirst($this->uri->segment(1))?></li>
+        <li><?php echo strtoupper($this->uri->segment(1))?></li>
         <li><?php echo ucfirst($sector2)?></li>
       </ol>
 </section>
